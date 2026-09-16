ee61fb13ad07e13c46605b85c093298b7533bcf7
chad <chad.yunitrade@gmail.com>
2026-09-16 15:05:54 +0800

    fix qr

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.screenbridge"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 37
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
